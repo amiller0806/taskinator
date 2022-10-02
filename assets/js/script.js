f2146c849f5e1b3f6cb8c92a7b6f4595662ad65f
amiller0806 <ariel.miller555@gmail.com>
2022-10-02 15:10:02 -0400

    capture form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,29 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function (event) {
+var createTaskHandler = function(event) {
   event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+
+  // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  // create div to hold task info and add to list item 
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+
+  // add HTML content to the div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class = 'task-type'>" + taskTypeInput + "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
+
+
 };
 
 formEl.addEventListener("submit", createTaskHandler);
-
-// Now the script.js file finds the <form> element in the page 
-// and saves it to the variable formEl, so that we can interact with the 
-// form and access some of its child HTML elements.
-// This particular listener listens for the following two events within the form:
-
-// When a user clicks a <button> element with a type attribute 
-// that has a value of "submit", like the button we currently have 
-// in the form
